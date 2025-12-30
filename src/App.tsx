b2ba6f8425fb967d93b2f06d6a3a1cc02524f8cf
Shanmukha Pothukuchi <shanmukha.pothukuchi@gmail.com>
2025-12-30 14:26:23 -0500

    radial menu component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,40 @@
 import './App.css'
+import RadialMenu from './components/RadialMenu'
 
 function App() {
   return (
-    <h1>Hello, World!</h1>
+    <RadialMenu
+      menu={[
+        { id: "defense", label: "Defense" },
+        {
+          id: "utility", label: "Utility",
+        },
+        {
+          id: "offense", label: "Offense",
+          children: [
+            {
+              id: "magic", label: "Magic",
+              children: [
+                { id: "fire", label: "Fire" },
+                { id: "ice", label: "Ice" },
+                { id: "lightning", label: "Lightning" },
+              ]
+            },
+            {
+              id: "melee", label: "Melee",
+            },
+            { id: "ranged", label: "Ranged" },
+          ]
+        },
+        { id: "support", label: "Support" }
+      ]}
+      onChange={(e) => {
+        console.log("Selected item:", e);
+      }}
+      onNavigation={(e) => {
+        console.log("Navigation path:", e);
+      }}
+    />
   )
 }
 
