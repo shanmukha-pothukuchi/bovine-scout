12662826c3bd2d5b2a4f1c29cc35f549ff2fa7b2
Shanmukha Pothukuchi <shanmukha.pothukuchi@gmail.com>
2026-01-07 15:53:23 -0500

    form editor layout

--- src/pages/config/FormEditor/index.tsx
+++ src/pages/config/FormEditor/index.tsx
@@ -3,7 +3,9 @@ import styles from "./index.module.css";
 export default function FormEditor() {
     return (
         <div className={styles.container}>
-            <div className={styles.sidebar}></div>
+            <div className={styles.left_sidebar}></div>
+            <div className={styles.main}></div>
+            <div className={styles.right_sidebar}></div>
         </div>
     );
 }
