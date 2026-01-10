b9b7a6b350c9d9d1e77b0b027ad6ca215d11df57
Shanmukha Pothukuchi <shanmukha.pothukuchi@gmail.com>
2026-01-10 06:36:28 -0500

    import shadcn

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { RouterProvider } from "react-router-dom";
+import { router } from "@/lib/router";
+import { ThemeProvider } from "@/components/theme-provider";
+
+export function App() {
+    return (
+        <ThemeProvider defaultTheme="dark">
+            <RouterProvider router={router} />
+        </ThemeProvider>
+    );
+}
+
+export default App;
