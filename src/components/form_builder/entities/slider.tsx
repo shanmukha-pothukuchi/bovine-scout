c7115849c0b0f37fb36112209b41bf5d37430b2e
Shanmukha Pothukuchi <shanmukha.pothukuchi@gmail.com>
2026-01-29 11:17:31 -0500

    use standard react plugin

--- src/components/form_builder/entities/slider.tsx
+++ src/components/form_builder/entities/slider.tsx
@@ -1,21 +1,73 @@
-import { Label } from "@/components/ui/label";
-import { makeAttribute, makeEntity } from "@/lib/form-builder";
-import { minAttr } from "@/components/form_builder/attributes/min";
-import { maxAttr } from "@/components/form_builder/attributes/max";
-import { z } from "zod";
-import { IconAdjustmentsAlt } from "@tabler/icons-react";
-import { Slider } from "@/components/ui/slider";
 import { labelAttr } from "@/components/form_builder/attributes/label";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Slider } from "@/components/ui/slider";
+import { makeAttribute, makeEntity } from "@/lib/form-builder";
+import { IconAdjustmentsAlt } from "@tabler/icons-react";
 import { useEffect } from "react";
+import { z } from "zod";
 
 export const sliderEntity = makeEntity({
   name: "Slider",
   icon: <IconAdjustmentsAlt />,
   attributes: {
     label: labelAttr,
-    min: minAttr,
-    max: maxAttr,
+    min: makeAttribute({
+      name: "min",
+      defaultValue: 0,
+      validate: (value: number) => {
+        const schema = z.number();
+        return schema.parse(value);
+      },
+      component: ({ value, setValue, validateValue, error }) => {
+        return (
+          <div className="w-full max-w-sm space-y-2">
+            <Label htmlFor="min">Min</Label>
+            <Input
+              id="min"
+              type="number"
+              placeholder="Min value"
+              value={value}
+              onChange={(e) => setValue(parseInt(e.target.value))}
+              onBlur={validateValue}
+            />
+            {error && (
+              <div className="text-destructive text-sm">
+                <span>{error}</span>
+              </div>
+            )}
+          </div>
+        );
+      },
+    }),
+    max: makeAttribute({
+      name: "max",
+      defaultValue: 10,
+      validate: (value: number) => {
+        const schema = z.number();
+        return schema.parse(value);
+      },
+      component: ({ value, setValue, validateValue, error }) => {
+        return (
+          <div className="w-full max-w-sm space-y-2">
+            <Label htmlFor="max">Max</Label>
+            <Input
+              id="max"
+              type="number"
+              placeholder="Max value"
+              value={value}
+              onChange={(e) => setValue(parseInt(e.target.value))}
+              onBlur={validateValue}
+            />
+            {error && (
+              <div className="text-destructive text-sm">
+                <span>{error}</span>
+              </div>
+            )}
+          </div>
+        );
+      },
+    }),
     defaultValue: makeAttribute({
       name: "defaultValue",
       defaultValue: 0,
